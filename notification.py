369496b129320108793e09300c9231ebc90dff9b
maumau <maumaumaumaumaumaumaumaumaumau@gmail.com>
2018-01-28 22:22:37 +0900

    update

--- notification.py
+++ notification.py
@@ -10,14 +10,6 @@ from email.utils import formatdate
 import json
 import requests
 
-"""
-only testing on Gmail account.
-change SMTP_ADDRESS and SMTP_PORT for your use.
-"""
-SMTP_ADDRESS = 'smtp.gmail.com'
-SMTP_PORT = 587   # for TLS
-#SMTP_PORT = 465  # for SSL
-
 class NotificationTemplate(object):
 	__metaclass__ = abc.ABCMeta
 
@@ -25,62 +17,76 @@ class NotificationTemplate(object):
 	def send_message(self, msg):
 		raise NotImplementedError()
 
+	@abc.abstractmethod
+	def contents(self):
+		raise NotImplementedError()
+
 class MailNotification(NotificationTemplate):
-    def __init__(self, passwd, from_addr, to_addr, bcc_addr, subject):
-    	"""
-    	from_addr = 'sender@gmail.com'
-    	passwd    = 'password'
-    	to_addr   = 'receiver@example.com'
-    	bcc_addr  = 'receiver2@example.com'
-    	subject   = 'test mail from python'
-    	body      = 'Hi! This mail is sent from python script!'
-    	"""
-        # set mail content
-        self._passwd = passwd
-        self._from_addr = from_addr
-        self._to_addr = to_addr
-        self._bcc_addr = bcc_addr
-        self._subject = subject
+	"""
+		only testing on Gmail account.
+		change SMTP_ADDRESS and SMTP_PORT for your use.
+	"""
+	SMTP_ADDRESS = 'smtp.gmail.com'
+	SMTP_PORT = 587   # for TLS
+	#SMTP_PORT = 465  # for SSL
+	
+	def __init__(self, passwd, from_addr, to_addr, bcc_addr, subject):
+		"""
+			from_addr = 'sender@gmail.com'
+			passwd    = 'password'
+			to_addr   = 'receiver@example.com'
+			bcc_addr  = 'receiver2@example.com'
+			subject   = 'test mail from python'
+			body      = 'Hi! This mail is sent from python script!'
+		"""
+		# set mail content
+		self._passwd = passwd
+		self._from_addr = from_addr
+		self._to_addr = to_addr
+		self._bcc_addr = bcc_addr
+		self._subject = subject
 
-        # Actually, I don't know what it should be...
-        self.charset = "iso-2022-jp"
+		# Actually, I don't know what it should be...
+		self.charset = "iso-2022-jp"
 
-    def _create_message(self, body):
-        msg = MIMEText(body, "plain", self.charset)
+	def _create_message(self, body):
+		msg = MIMEText(body, "plain", self.charset)
 
-        msg['Subject'] = self._subject
-        msg['From'] = self._from_addr
-        msg['To'] = self._to_addr
-        msg['Bcc'] = self._bcc_addr
-        msg['Date'] = formatdate()
+		msg['Subject'] = self._subject
+		msg['From'] = self._from_addr
+		msg['To'] = self._to_addr
+		msg['Bcc'] = self._bcc_addr
+		msg['Date'] = formatdate()
 
-        return msg
+		return msg
 
-    def send_message(self, msg):
-        mail_content = self._create_message(msg)
+	def send_message(self, msg):
+		mail_content = self._create_message(msg)
 
-        try:
-        	# expecting using Gmail
-            smtpobj = smtplib.SMTP(SMTP_ADDRESS, SMTP_PORT)
-            smtpobj.ehlo()
-            smtpobj.starttls()
-            smtpobj.ehlo()
-            smtpobj.login(self._from_addr, self._passwd)
-            smtpobj.sendmail(self._from_addr, self._to_addr, mail_content.as_string())
-            smtpobj.close()
-        except Exception as e:
-            import traceback
-            traceback.print_exc()
-            print(e)
-            print("which means, could not send a notification mail...")
+		try:
+			# expecting using Gmail
+			smtpobj = smtplib.SMTP(self.SMTP_ADDRESS, self.SMTP_PORT)
+			smtpobj.ehlo()
+			smtpobj.starttls()
+			smtpobj.ehlo()
+			smtpobj.login(self._from_addr, self._passwd)
+			smtpobj.sendmail(self._from_addr, self._to_addr, mail_content.as_string())
+			smtpobj.close()
+		except Exception as e:
+			import traceback
+			traceback.print_exc()
+			print(e)
+			print("which means, could not send a notification mail...")
+	def contents(self):
+		return "pass:********, account:{}, to:{}, bcc:{}, subject:{}".format(self._from_addr, self._to_addr, self._bcc_addr, self._subject)
 
 class SlackNotification(NotificationTemplate):
 	def __init__(self, user_name, channel, hook_url):
 		"""
-    	user_name = 'user_name'
-    	channel   = 'which channel you want to post'
-    	hook_url  = 'api's web hook url
-    	"""
+			user_name = 'user_name'
+			channel   = 'which channel you want to post'
+			hook_url  = 'api's web hook url
+		"""
 		self.user_name = user_name
 		self.channel = channel
 		self.hook_url = hook_url
@@ -101,7 +107,7 @@ class SlackNotification(NotificationTemplate):
 		content = {"username" : self.user_name,
 				   "channel"  : self.channel,
 				   "text"     : msg
-                   }
+				   }
 
 		try:
 			resp = requests.post(self.hook_url, data=json.dumps(content))
@@ -113,3 +119,6 @@ class SlackNotification(NotificationTemplate):
 			import traceback
 			traceback.print_exc()
 			print(e)
+
+	def contents(self):
+		return "user_name:{}, channel:{}".format(self.user_name, self.channel)
--- notificator.py
+++ notificator.py
@@ -1,3 +1,6 @@
+# in case python2
+from __future__ import print_function
+
 import threading
 
 from .notification import MailNotification, SlackNotification
@@ -7,13 +10,6 @@ class Notificator:
 	def __init__(self):
 		self._notificators = []
 
-	# set default Slack notification, which is written in secret.py
-	def setSlack(self):
-		self._notificators.append(SlackNotification(secret.SLACK_USER_NAME, secret.SLACK_CHANNEL, secret.SLACK_HOOK_URL))
-
-	def addSlackNotify(self, user_name, channel, hook_url):
-		self._notificators.append(SlackNotification(user_name, channel, hook_url))
-
 	# set default mail notification, which is written in secret.py
 	def setMail(self):
 		self._notificators.append(MailNotification(secret.MAIL_PASSWORD, secret.MAIL_ACCOUNT, secret.MAIL_TO_ADDRESS, secret.MAIL_BCC_ADDRESS, secret.MAIL_SUBJECT))
@@ -21,10 +17,30 @@ class Notificator:
 	def addMailNotify(self, passwd, account, to_addr, bcc_addr, subject):
 		self._notificators.append(MailNotification(passwd, account, to_addr, bcc_addr, subject))
 
+	# set default Slack notification, which is written in secret.py
+	def setSlack(self):
+		self._notificators.append(SlackNotification(secret.SLACK_USER_NAME, secret.SLACK_CHANNEL, secret.SLACK_HOOK_URL))
+
+	def addSlackNotify(self, user_name, channel, hook_url):
+		self._notificators.append(SlackNotification(user_name, channel, hook_url))
+
+	def show_list(self):
+		for idx, noti in enumerate(self._notificators):
+			print("{:3} : {}".format(idx, noti.contents()))
+
+	def del_notify(self, idx):
+		if idx < len(self._notificators) and idx >= 0:
+			_ = self._notificators.pop(idx)
+			return True
+		return False
+
+	def get_list(self):
+		return self._notificators
+
 	def _send_notification(self, msg):
 		# broadcast notification
-		for idx in range(len(self._notificators)):
-			self._notificators[idx].send_message(msg)
+		for noti in self._notificators:
+			noti.send_message(msg)
 
 	def notify(self, msg, use_thread=False):
 		# sometimes it take time, so if you want low latency, set use_thread=True
