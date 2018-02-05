7a1e7800b591c00b697a9f7af6e80ce7a5be9596
maumau <maumaumaumaumaumaumaumaumaumau@gmail.com>
2018-02-05 20:11:01 +0900

    new

---
+++ setup_alias.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+set -Ceu
+
+INS_DIR="$HOME"/.scripts
+INS_SCRIPT="notify.py"
+RC_FILE=0
+
+# find default shell rc file of major shells
+if [[ $(echo $SHELL | grep bash) ]]; then
+    RC_FILE=$HOME/.bashrc
+elif [[ $(echo $SHELL | grep zsh) ]]; then
+    RC_FILE=$HOME/.zshrc
+elif [[ $(echo $SHELL | grep csh) ]]; then
+    RC_FILE=$HOME/.cshrc
+elif [[ $(echo $SHELL | grep ksh) ]]; then
+    RC_FILE=$HOME/.kshrc
+elif [[ $(echo $SHELL | grep tcsh) ]]; then
+    RC_FILE=$HOME/.tcshrc
+elif [[ $(echo $SHELL | grep ash) ]]; then
+    RC_FILE=$HOME/.ashrc
+elif [[ $(echo $SHELL | grep sh) ]]; then
+    RC_FILE=$HOME/.profile
+else
+	echo "Non-standard shell $SHELL detected. sorry"
+	echo "see the source and setup by yourself XD"
+fi
+
+if [ -f "$RC_FILE" ]; then
+	mkdir -p "$INS_DIR"
+	cp "$RC_FILE" "$RC_FILE".notify.back
+	cp ./"$INS_SCRIPT" "$INS_DIR"/
+	chmod 755 "$INS_DIR"/"$INS_SCRIPT"
+
+	# please use ">>" not ">"
+	echo ""                                    >> "$RC_FILE"
+	echo "function notify() {"                 >> "$RC_FILE"
+	echo "    python $INS_DIR/$INS_SCRIPT \$@" >> "$RC_FILE"
+	echo "}"                                   >> "$RC_FILE"
+
+	source "$RC_FILE"
+fi
